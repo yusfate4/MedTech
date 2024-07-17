3df08c2725cb8aa0ca3e859a5440235ed80ba938
Dr-Bernard <drsunny3030@gmail.com>
2024-07-17 05:13:39 +0100

    I just customized the page title etc

--- consult/admin.py
+++ consult/admin.py
@@ -1,7 +1,55 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ["pub_date", "question_text"]
 
-admin.site.register(Question)
 
+# admin.site.register(Question, QuestionAdmin)
+
+
+# You’ll follow this pattern – create a model admin class, then pass it as the second argument to admin.site.register() – any time you need to change the admin options for a model.
+
+# This particular change above makes the “Publication date” come before the “Question” field.
+
+# Split the form up into fieldsets as follows:
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {"fields": ["question_text"]}),
+#         ("Date information", {"fields": ["pub_date"]}),
+#     ]
+
+# admin.site.register(Question, QuestionAdmin)
+
+# Now also register Choice with the admin just as we did with Question:
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Choice)
+
+# Better still, let us add a bunch of Choices directly when we create the Question object as follows:
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    list_filter = ["pub_date"]
+
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)

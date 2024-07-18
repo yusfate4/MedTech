303c84bd6d33a96208a8a2581b8a4508944d21b3
Dr-Bernard <drsunny3030@gmail.com>
2024-07-18 02:29:24 +0100

    I just enabled realtime debugging using django-debug-toolbar.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('consult/', include('consult.urls')),
     path('admin/', admin.site.urls),
-]
+] + debug_toolbar_urls()

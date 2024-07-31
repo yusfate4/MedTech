52a036d517900ed043d5660acdf51885adc084c5
Dr-Bernard <drsunny3030@gmail.com>
2024-07-31 05:59:53 +0100

    I just enabled Cross-Origin Resource Sharing to allow Frontend run the app on different server or port.

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # or the URL where frontend is running
+]
+
 
 ROOT_URLCONF = 'mysite.urls'
 
